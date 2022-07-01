262372c08a046b4a4e53c66ca53d530ea1e2d102
Teerachai Laothong <teerachai@perfenterprise.com>
2022-07-02 01:16:19 +0700

    Just add client lib and wpf test client

---
+++ InformaLink.Client/InformationLinkClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flurl.Http;
+
+namespace InformaLink.Client
+{
+    public class InformationLinkClient
+    {
+        public async Task GetInfo()
+        {
+            await "https://localhost:7058/api/primary".GetJsonAsync<List<PrimaryRecord>>();
+        }
+    }
+}
